0b6b0a34204b076f33497261f5072ed0dc5d0f3e
grrramoss <139362785+grrramoss@users.noreply.github.com>
2023-09-06 14:33:30 -0300

    Adicionar arquivos de projeto.

---
+++ Produtos/Models/Conexao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Produtos.Models
+{
+    public class Conexao : DbContext
+    {
+        public Conexao() : base("Produtos")
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+    }
+}
